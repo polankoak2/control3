5e497be189328f9c97a6ec77ed68bf6f186b94e0
polankoak2 <polankoak@yahoo.es>
2023-10-29 13:24:47 -0300

    primera subida

---
+++ new_control3/new_control3/Module1.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Runtime.CompilerServices
+
+Module Module1
+
+    Sub Main()
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Dim suma As Integer
+        Console.WriteLine("Introduzca su primer numero: ")
+        num1 = Console.ReadLine()
+        If (num1 Mod 2 = 1) Then
+            Console.WriteLine("Numero Impar, continue con el siguiente...")
+        Else
+            Console.WriteLine("Numero Par, por favor ingresar otro numero")
+
+        End If
+
+        Console.WriteLine("Introduzca su segundo numero: ")
+        num2 = Console.ReadLine()
+
+        If (num2 Mod 2 = 1) Then
+            Console.WriteLine("Numero Impar, continue con el siguiente...")
+        Else
+            Console.WriteLine("Numero Par, por favor ingresar otro numero")
+        End If
+
+        suma = num1 + num2
+
+        Console.WriteLine("La suma de los numeros impares es: " & suma)
+        Console.ReadLine()
+    End Sub
+
+End Module
